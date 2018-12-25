a960812c5b4bffc75ed8b53fb6fb4eae51683d09
krasilovamaria <marivasi11@yandex.ru>
2018-12-25 14:58:11 +0700

    Проверка пред защитой

--- js/sedona.js
+++ js/sedona.js
@@ -1,7 +1,11 @@
-var link = document.querySelector(".search-button");
+var link = document.querySelector(".search-btn");
 var searchform = document.querySelector(".search");
 
 link.addEventListener("click", function (evt) {
+
+  evt.preventDefault();
+
+  searchform.classList.toggle('search-open');
 
 
 });
