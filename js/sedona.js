e6769c9ceaf4693d6a3eb47abfd67cee4907b9a6
krasilovamaria <marivasi11@yandex.ru>
2018-12-29 15:00:26 +0700

    Первые правки после проверки

--- js/sedona.js
+++ js/sedona.js
@@ -2,12 +2,6 @@ var link = document.querySelector(".search-btn");
 var searchform = document.querySelector(".search");
 
 link.addEventListener("click", function (evt) {
-
   evt.preventDefault();
-
   searchform.classList.toggle('search-open');
-
-
 });
-
-
